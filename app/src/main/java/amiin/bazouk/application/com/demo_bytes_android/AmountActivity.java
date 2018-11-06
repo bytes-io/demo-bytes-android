ed1315cd41b4bb742c5b20a4e03cc992cfd647fc
Bazouk55555 <adrien_boukobza@hotmail.fr>
2018-11-06 16:32:20 +0000

    remove the problem of asynchronous

--- app/src/main/java/amiin/bazouk/application/com/demo_bytes_android/AmountActivity.java
+++ app/src/main/java/amiin/bazouk/application/com/demo_bytes_android/AmountActivity.java
@@ -5,7 +5,6 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.text.Editable;
 import android.text.TextWatcher;
-import android.view.KeyEvent;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.TextView;
@@ -18,12 +17,25 @@ import amiin.bazouk.application.com.demo_bytes_android.iota.ApplyTransaction;
 
 public class AmountActivity extends AppCompatActivity {
 
-    private double rate;
+    private double rate = -1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_amount);
+        Thread conversionThread = new Thread(new Runnable() {
+            @Override
+            public void run() {
+                try {
+                    rate = ApplyTransaction.getPriceUSD();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                } catch (ParseException e) {
+                    e.printStackTrace();
+                }
+            }
+        });
+        conversionThread.start();
 
         findViewById(R.id.ok).setOnClickListener(new View.OnClickListener(){
             @Override
@@ -47,31 +59,9 @@ public class AmountActivity extends AppCompatActivity {
             @Override
             public void afterTextChanged(Editable s) {
                 if(!s.toString().isEmpty()) {
-                    Thread convertToUsdThread = new Thread(new Runnable() {
-                        @Override
-                        public void run() {
-                            rate = -1;
-                            try {
-                                rate = ApplyTransaction.getPriceUSD();
-                            } catch (IOException e) {
-                                e.printStackTrace();
-                            } catch (ParseException e) {
-                                e.printStackTrace();
-                            }
-                            while (rate == -1) {
-                                if (rate != -1) {
-                                    break;
-                                }
-                            }
-                            runOnUiThread(new Runnable() {
-                                @Override
-                                public void run() {
-                                    ((TextView) findViewById(R.id.usd)).setText(String.valueOf(Double.valueOf(s.toString()) * rate));
-                                }
-                            });
-                        }
-                    });
-                    convertToUsdThread.start();
+                    if(rate!=-1) {
+                        ((TextView) findViewById(R.id.usd)).setText(String.valueOf(Double.valueOf(s.toString()) * rate));
+                    }
                 }
                 else{
                     ((TextView) findViewById(R.id.usd)).setText("");
